4fabd1ae0896e6c2bb09d69d037dbd7eff9560c8
Sami Aalto <sami.aalto@tuni.fi>
2019-02-09 16:04:08 +0200

    Change server port

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { getCoordinates } = require("./databaserequests");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/api/v1/parking-areas", async (req, res) => {
   try {
