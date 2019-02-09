c8738c0a82478c417d504ee375e95d6b19e94d1b
jesushilden <peter.e.hilden@gmail.com>
2019-02-09 13:43:08 +0200

    Polygon drawing on map to visualize parking areas

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Polygon } from 'google-maps-react';
 import { ENV } from "../environment";
 
 class MapContainer extends Component {
     render() {
         return (
             <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={{
-             lat: 60.192059,
-             lng: 24.945831
-            }}
-          />
-        );
+                google={this.props.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={{
+                    lat: 60.192059,
+                    lng: 24.945831
+                }}
+            >
+                {this.props.areas.map(area =>
+                    <Polygon
+                        paths={area.coordinates}
+                        strokeColor="#0000FF"
+                        strokeOpacity={0.8}
+                        strokeWeight={2}
+                        fillColor="#0000FF"
+                        fillOpacity={0.35} />
+                )}
+            </Map>
+        )
     }
 }
 
 const mapStyles = {
     width: '50%',
     height: '100%'
-  };
+};
 
-  export default GoogleApiWrapper({
+export default GoogleApiWrapper({
     apiKey: ENV.APIKEY
-  })(MapContainer);
+})(MapContainer);
