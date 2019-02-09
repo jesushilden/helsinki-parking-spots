2fef8ea897400515149d73c7f73e04b56627c467
Sami Aalto <sami.aalto@tuni.fi>
2019-02-09 15:11:30 +0200

    Fix path issue in models.js

--- server/src/models.js
+++ server/src/models.js
@@ -1,25 +1,26 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const path = require("path");
 
 const sequelize = new Sequelize(null, null, null, {
-	dialect: 'sqlite',
-	storage: './db/parking.db',
+  dialect: "sqlite",
+  storage: path.resolve(__dirname, "db/parking.db")
 });
 
-const Area = sequelize.define('parkingareas', {
-	areaId: Sequelize.STRING,
-	capacity: Sequelize.INTEGER,
+const Area = sequelize.define("parkingareas", {
+  areaId: Sequelize.STRING,
+  capacity: Sequelize.INTEGER
 });
 
-const Coordinate = sequelize.define('coordinates', {
-	areaId: Sequelize.STRING,
-	lat: Sequelize.FLOAT,
-	long: Sequelize.FLOAT,
+const Coordinate = sequelize.define("coordinates", {
+  areaId: Sequelize.STRING,
+  lat: Sequelize.FLOAT,
+  long: Sequelize.FLOAT
 });
 
 Area.sync();
 Coordinate.sync();
 
 module.exports = {
-	Area,
-	Coordinate,
+  Area,
+  Coordinate
 };
